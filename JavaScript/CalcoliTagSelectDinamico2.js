b8461b69c7d2330bc66b51ffff9e380ce768aa62
GattoTommaso <tommaso.gatto.03@gmail.com>
2021-04-23 18:25:29 +0200

    tommaso

---
+++ JavaScript/CalcoliTagSelectDinamico2.js
@@ -0,0 +1,163 @@
+function nascondi(){
+    var apri;
+
+    apri = document.getElementById("domanda_1");
+    if (apri.style.display == "none") {
+        document.getElementById("domanda_1").style.display = "block";
+    } else {
+        document.getElementById("domanda_1").style.display = "none";
+    }
+    
+    apri = document.getElementById("domanda_2");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+    
+    apri = document.getElementById("domanda_3");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_4");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_5");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_6");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_7");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_8");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_9");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_10");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_11");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_12");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+
+    apri = document.getElementById("domanda_13");
+    if (apri.style.display == "none") {
+        apri.style.display = "block";
+    } else {
+        apri.style.display = "none";
+    }
+}
+
+function nascondiTutto(){
+    var x;
+
+    x= document.getElementById("domanda_1");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_2");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_3");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_4");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_5");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_6");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_7");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_8");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_9");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_10");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_11");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_12");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+
+    x= document.getElementById("domanda_13");
+    if(x.style.display != "none"){
+        x.style.display = "none";
+    }
+}
