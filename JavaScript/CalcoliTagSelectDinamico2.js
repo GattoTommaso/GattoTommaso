61c17fa7a5d07a5eae52e06c49442ae2b8f3abbe
GattoTommaso <tommaso.gatto.03@gmail.com>
2021-05-02 11:12:52 +0200

    t

--- JavaScript/CalcoliTagSelectDinamico2.js
+++ JavaScript/CalcoliTagSelectDinamico2.js
@@ -1,92 +1,153 @@
-function nascondi(){
+function nascondi1(){
     var apri;
 
     apri = document.getElementById("domanda_1");
     if (apri.style.display == "none") {
-        document.getElementById("domanda_1").style.display = "block";
+        nascondiTutto();
+        apri.style.display = "block";
     } else {
-        document.getElementById("domanda_1").style.display = "none";
+        apri.style.display = "none";
     }
-    
+}
+
+function nascondi2(){
+    var apri;
+
     apri = document.getElementById("domanda_2");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
-    
+}
+
+function nascondi3(){
+    var apri;
+
     apri = document.getElementById("domanda_3");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
+
+function nascondi4(){
+    var apri;
 
     apri = document.getElementById("domanda_4");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
+
+function nascondi5(){
+    var apri;
 
     apri = document.getElementById("domanda_5");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
+
+function nascondi6(){
+    var apri;
 
     apri = document.getElementById("domanda_6");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
+
+function nascondi7(){
+    var apri;
 
     apri = document.getElementById("domanda_7");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
+
+function nascondi8(){
+    var apri;
 
     apri = document.getElementById("domanda_8");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
+
+function nascondi9(){
+    var apri;
 
     apri = document.getElementById("domanda_9");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
+
+function nascondi10(){
+    var apri;
 
     apri = document.getElementById("domanda_10");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
+
+function nascondi11(){
+    var apri;
 
     apri = document.getElementById("domanda_11");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
 
+function nascondi12(){
+    var apri;
+    
     apri = document.getElementById("domanda_12");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
     }
+}
 
+function nascondi13(){
+    var apri;
+    
     apri = document.getElementById("domanda_13");
     if (apri.style.display == "none") {
+        nascondiTutto();
         apri.style.display = "block";
     } else {
         apri.style.display = "none";
